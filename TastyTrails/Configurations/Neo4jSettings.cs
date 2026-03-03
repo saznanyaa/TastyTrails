9b4241b4c44d96b2099db7881b0712e9341c4b6b
David <david.stanojevic@elfak.rs>
2026-03-03 11:54:59 +0100

    Dodata neo4j konfiguracija i klasa za podešavanje

---
+++ TastyTrails/Configurations/Neo4jSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TastyTrails.Configurations;
+
+public class Neo4jSettings
+{
+    public string Uri { get; set; } = null!;
+    public string User { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
